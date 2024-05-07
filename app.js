663e851d0262ffd22c3ddba6e5838f2e13156d3f
nishukundar <nischithasrinivas83@gmail.com>
2024-05-07 07:51:00 +0530

    selecting dropdown option

---
+++ app.js
@@ -0,0 +1,19 @@
+const BASE_URL = 
+"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json ";
+ 
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        } else if  (select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+}
